05ec0eb10c401c7078ffad5a993e16c5aecfeb1e
MohamedElmdary <engm5081@gmail.com>
2022-04-05 15:22:02 +0200

    Added error & loading handling in GovView

--- example/frontend/src/views/GovView.vue
+++ example/frontend/src/views/GovView.vue
@@ -18,9 +18,26 @@
       />
 
       <v-row justify="center">
-        <v-btn color="primary" type="submit"> Submit </v-btn>
+        <v-btn
+          color="primary"
+          type="submit"
+          :disabled="loading"
+          :loading="loading"
+        >
+          Submit
+        </v-btn>
       </v-row>
     </form>
+
+    <v-alert
+      class="mt-10"
+      :type="error ? 'error' : 'success '"
+      border="left"
+      outlined
+      v-if="!loading && (result || error)"
+    >
+      {{ result || error }}
+    </v-alert>
   </v-container>
 </template>
 
@@ -33,24 +50,41 @@ import { BigNumber } from "ethers";
   name: "GovView",
 })
 export default class GovView extends Vue {
+  loading = false;
+  result: any = null;
+  error: string | null = null;
+
   title = "";
   description = "";
   initialDeposit = 0;
 
   onSubmitProposal() {
-    const { title, description, initialDeposit } = this;
-    submitProposal(
-      this.$store.state.config.tendermint_rpc,
-      { title, description },
-      BigNumber.from(initialDeposit),
-      this.$store.state.config.proposal_denom
-    )
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log("Error", err);
-      });
+    this.loading = true;
+    this.result = null;
+    this.error = null;
+
+    try {
+      const { title, description, initialDeposit } = this;
+      submitProposal(
+        this.$store.state.config.tendermint_rpc,
+        { title, description },
+        BigNumber.from(initialDeposit),
+        this.$store.state.config.proposal_denom
+      )
+        .then((res) => {
+          this.result = "Transaction submitted succefully!";
+        })
+        .catch((err) => {
+          console.log("Error", err);
+          this.error = err.message;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    } catch (err: any) {
+      this.loading = false;
+      this.error = err.message;
+    }
   }
 }
 </script>
