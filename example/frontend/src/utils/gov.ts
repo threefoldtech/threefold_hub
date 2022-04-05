dc2308d4aa3d9a85a4c56c782e7cb1fd3f24d906
OmarElawady <omarelawady1998@gmail.com>
2022-04-05 12:31:34 +0200

    add more gov ops

--- example/frontend/src/utils/gov.ts
+++ example/frontend/src/utils/gov.ts
@@ -1,26 +1,15 @@
-import { CosmosGovV1Beta1QueryProposalsResponse, Api } from "@/rest/cosmos";
+import { CosmosGovV1Beta1QueryProposalsResponse, Api, CosmosGovV1Beta1QueryProposalResponse, CosmosGovV1Beta1QueryParamsResponse, CosmosGovV1Beta1QueryVotesResponse, CosmosGovV1Beta1QueryDepositsResponse } from "@/rest/cosmos";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { snakeToCamelCase } from "./camel"
 import { myRegistry } from "./registry";
-import { MsgSubmitProposal, MsgVote } from "../types/cosmos/gov/v1beta1/tx";
+import { MsgDeposit, MsgSubmitProposal, MsgVote } from "../types/cosmos/gov/v1beta1/tx";
 import { TextProposal, VoteOption } from "@/types/cosmos/gov/v1beta1/gov";
 import { Any } from "@/types/google/protobuf/any";
 import { BigNumber } from "ethers";
 import { Coin } from "@/types/cosmos/base/v1beta1/coin";
 import { GAS, FEE, FEE_DENOM } from "./fees"
 
-/*
-    proto.RegisterType((*MsgSubmitProposal)(nil), "cosmos.gov.v1beta1.MsgSubmitProposal")
-    proto.RegisterType((*MsgSubmitProposalResponse)(nil), "cosmos.gov.v1beta1.MsgSubmitProposalResponse")
-    proto.RegisterType((*MsgVote)(nil), "cosmos.gov.v1beta1.MsgVote")
-    proto.RegisterType((*MsgVoteResponse)(nil), "cosmos.gov.v1beta1.MsgVoteResponse")
-    proto.RegisterType((*MsgVoteWeighted)(nil), "cosmos.gov.v1beta1.MsgVoteWeighted")
-    proto.RegisterType((*MsgVoteWeightedResponse)(nil), "cosmos.gov.v1beta1.MsgVoteWeightedResponse")
-    proto.RegisterType((*MsgDeposit)(nil), "cosmos.gov.v1beta1.MsgDeposit")
-    proto.RegisterType((*MsgDepositResponse)(nil), "cosmos.gov.v1beta1.MsgDepositResponse")
-*/
-
-export async function listProposals(
+async function listProposals(
     cosmos_rest: string,
     status?:
         | "PROPOSAL_STATUS_UNSPECIFIED"
@@ -35,8 +24,6 @@ export async function listProposals(
     if (!window.keplr) {
         throw new Error("keplr is not installed");
     }
-    const signer = window.keplr.getOfflineSigner("threefold-hub");
-    const sender = (await signer.getAccounts())[0].address;
     const queryClient = new Api({ baseUrl: cosmos_rest });
     const response = await queryClient.cosmos.cosmosGovV1Beta1Proposals(
         {
@@ -50,7 +37,62 @@ export async function listProposals(
     return response.data as CosmosGovV1Beta1QueryProposalsResponse;
 }
 
-export async function submitProposal(
+async function listVotes(
+    cosmos_rest: string,
+    proposalId: string,
+    offset?: number,
+    limit?: number,
+): Promise<CosmosGovV1Beta1QueryVotesResponse> {
+    if (!window.keplr) {
+        throw new Error("keplr is not installed");
+    }
+    const queryClient = new Api({ baseUrl: cosmos_rest });
+    const response = await queryClient.cosmos.cosmosGovV1Beta1Votes(proposalId, {
+        "pagination.limit": limit?.toString(),
+        "pagination.offset": offset?.toString(),
+    }, { format: "json" });
+    snakeToCamelCase(response.data)
+    return response.data as CosmosGovV1Beta1QueryVotesResponse;
+}
+
+async function listDeposites(
+    cosmos_rest: string,
+    proposalId: string,
+    offset?: number,
+    limit?: number,
+): Promise<CosmosGovV1Beta1QueryDepositsResponse> {
+    if (!window.keplr) {
+        throw new Error("keplr is not installed");
+    }
+    const queryClient = new Api({ baseUrl: cosmos_rest });
+    const response = await queryClient.cosmos.cosmosGovV1Beta1Deposits(proposalId, {
+        "pagination.limit": limit?.toString(),
+        "pagination.offset": offset?.toString(),
+    }, { format: "json" });
+    snakeToCamelCase(response.data)
+    return response.data as CosmosGovV1Beta1QueryDepositsResponse;
+}
+
+async function parameters(
+    cosmos_rest: string,
+): Promise<CosmosGovV1Beta1QueryParamsResponse> {
+    if (!window.keplr) {
+        throw new Error("keplr is not installed");
+    }
+    const queryClient = new Api({ baseUrl: cosmos_rest });
+    // requesting all params is not supported over rest
+    const params = await queryClient.cosmos.cosmosGovV1Beta1Params("deposit", {format: "json"});
+    const tallying_params = await queryClient.cosmos.cosmosGovV1Beta1Params("tallying", {format: "json"});
+    const voting_params = await queryClient.cosmos.cosmosGovV1Beta1Params("voting", {format: "json"});
+    snakeToCamelCase(params.data)
+    snakeToCamelCase(tallying_params.data)
+    snakeToCamelCase(voting_params.data)
+    params.data.tallyParams = tallying_params.data.tallyParams
+    params.data.votingParams = tallying_params.data.votingParams
+    return params.data as CosmosGovV1Beta1QueryParamsResponse;
+}
+
+async function submitProposal(
     tendermint_rpc: string,
     content: TextProposal,
     initialDeposit: BigNumber,
@@ -63,7 +105,6 @@ export async function submitProposal(
     const offlineSigner = window.keplr.getOfflineSigner("threefold-hub");
     const sender = (await offlineSigner.getAccounts())[0];
     let client: any;
-    console.log(sender);
     return SigningStargateClient.connectWithSigner(
         tendermint_rpc, // Replace with your own RPC endpoint
         offlineSigner,
@@ -95,15 +136,60 @@ export async function submitProposal(
                 ],
                 gas: GAS,
             };
-            console.log(sender);
-            console.log(message);
             return client.signAndBroadcast(account.bech32Address, [message], fee);
         });
 
     // TODO: how to check transaction errors
 }
 
-export async function submitVote(
+async function deposit(
+    tendermint_rpc: string,
+    proposalId: string,
+    deposit: BigNumber,
+    denom: string
+) {
+    if (!window.keplr) {
+        throw new Error("keplr is not installed");
+    }
+    // TODO: should this be done globally one time?
+    const offlineSigner = window.keplr.getOfflineSigner("threefold-hub");
+    const sender = (await offlineSigner.getAccounts())[0];
+    let client: any;
+    return SigningStargateClient.connectWithSigner(
+        tendermint_rpc, // Replace with your own RPC endpoint
+        offlineSigner,
+        { registry: myRegistry }
+    )
+        .then((_client) => (client = _client))
+        .then(() => window.keplr.getKey("threefold-hub"))
+        .then((account) => {
+            const message = {
+                typeUrl: "/cosmos.gov.v1beta1.MsgDeposit", // Same as above
+                value: MsgDeposit.fromPartial({
+                    amount: [Coin.fromPartial({
+                        amount: deposit.toString(),
+                        denom: denom
+                    })],
+                    depositor: sender,
+                    proposalId: proposalId
+                }),
+            };
+            const fee = {
+                amount: [
+                    {
+                        denom: FEE_DENOM, // Use the appropriate fee denom for your chain
+                        amount: FEE,
+                    },
+                ],
+                gas: GAS,
+            };
+            return client.signAndBroadcast(account.bech32Address, [message], fee);
+        });
+
+    // TODO: how to check transaction errors
+}
+
+async function submitVote(
     tendermint_rpc: string,
     proposalId: string,
     vote: VoteOption
@@ -115,7 +201,6 @@ export async function submitVote(
     const offlineSigner = window.keplr.getOfflineSigner("threefold-hub");
     const sender = (await offlineSigner.getAccounts())[0];
     let client: any;
-    console.log(sender);
     return SigningStargateClient.connectWithSigner(
         tendermint_rpc, // Replace with your own RPC endpoint
         offlineSigner,
@@ -146,4 +231,13 @@ export async function submitVote(
 
     // TODO: how to check transaction errors
 }
-  // cosmos.gov.v1beta1.TextProposal
+
+export {
+    deposit,
+    listProposals,
+    listVotes,
+    listDeposites,
+    parameters,
+    submitProposal,
+    submitVote
+}
