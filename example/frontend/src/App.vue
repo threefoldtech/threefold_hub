8d8039cc917af2109396cc7a37f6c846afb68941
MohamedElmdary <engm5081@gmail.com>
2022-04-07 12:08:13 +0200

    Added check for keplr

--- example/frontend/src/App.vue
+++ example/frontend/src/App.vue
@@ -24,6 +24,7 @@
     <v-main style="padding-top: 100px">
       <router-view />
     </v-main>
+    <!-- </div> -->
   </v-app>
 </template>
 
@@ -34,6 +35,9 @@ import { Component, Vue } from "vue-property-decorator";
   name: "App",
 })
 export default class App extends Vue {
+  loading = true;
+  error = false;
+
   routes = [
     { label: "Send to Cosmos", path: "/" },
     { label: "Send to BSC", path: "/bsc" },
@@ -41,5 +45,32 @@ export default class App extends Vue {
     { label: "Pending BSC transactions", path: "/list-bsc" },
     { label: "Proposals", path: "/list-proposals" },
   ];
+
+  created() {
+    this.__checkKeplr()
+      .then(() => {
+        this.loading = false;
+      })
+      .catch(() => {
+        this.error = true;
+      });
+  }
+
+  private __checkKeplr(): Promise<any> {
+    let i = 0;
+
+    return new Promise((res, rej) => {
+      function _checkKeplr() {
+        if (window.keplr) res(window.keplr);
+        else if (i === 10) {
+          rej();
+        } else {
+          i++;
+          setTimeout(_checkKeplr, 500);
+        }
+      }
+      _checkKeplr();
+    });
+  }
 }
 </script>
