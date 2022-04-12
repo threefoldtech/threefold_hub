7adcd258fd15516aff6ad79359f92a0b0e5e8cdd
OmarElawady <omarelawady1998@gmail.com>
2022-04-12 12:48:05 +0200

    add gov frontend

---
+++ example/frontend/src/views/GovView.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-container>
+    <h1>Add Proposal</h1>
+
+    <form @submit.prevent="onSubmitProposal()">
+      <v-text-field label="Title" placeholder="Title" v-model="title" />
+
+      <v-text-field
+        label="Initial Deposit"
+        placeholder="Initial Deposit"
+        type="number"
+        v-model="initialDeposit"
+      />
+
+      <v-textarea
+        label="Description"
+        placeholder="Description"
+        v-model="description"
+        hint="Markdown is supported"
+      />
+
+      <v-row justify="center">
+        <v-btn
+          color="primary"
+          type="submit"
+          :disabled="loading"
+          :loading="loading"
+        >
+          Submit
+        </v-btn>
+      </v-row>
+    </form>
+
+    <CustomAlert :loading="loading" :result="result" :error="error" />
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { submitProposal } from "@/utils/gov";
+import { BigNumber } from "ethers";
+import CustomAlert from "@/components/CustomAlert.vue";
+
+@Component({
+  name: "GovView",
+  components: {
+    CustomAlert,
+  },
+})
+export default class GovView extends Vue {
+  loading = false;
+  result: any = null;
+  error: string | null = null;
+
+  title = "";
+  description = "";
+  initialDeposit = 0;
+
+  onSubmitProposal() {
+    this.loading = true;
+    this.result = null;
+    this.error = null;
+
+    try {
+      const { title, description, initialDeposit } = this;
+      submitProposal(
+        this.$store.state.config.tendermint_rpc,
+        { title, description },
+        BigNumber.from(initialDeposit),
+        this.$store.state.config.proposal_denom
+      )
+        .then((res) => {
+          this.result = "Proposal added succefully!";
+        })
+        .catch((err) => {
+          console.log("Error", err);
+          this.error = err.message;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    } catch (err: any) {
+      this.loading = false;
+      this.error = err.message;
+    }
+  }
+}
+</script>
