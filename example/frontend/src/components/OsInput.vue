cbc3454d2ad220612ded63618349c3b041b5bf87
MohamedElmdary <engm5081@gmail.com>
2022-04-19 11:42:14 +0200

    Added software proposal

--- example/frontend/src/components/OsInput.vue
+++ example/frontend/src/components/OsInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-row>
+  <div style="display: flex">
     <div style="width: 100px">
       <v-select
         label="OS"
@@ -15,18 +15,43 @@
       />
     </div>
     <v-text-field label="Url" v-model="url" />
-  </v-row>
+    <v-btn
+      fab
+      small
+      color="error"
+      class="ml-2"
+      @click="$emit('on:remove')"
+      v-if="removable"
+    >
+      <v-icon> mdi-minus </v-icon>
+    </v-btn>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 
 @Component({
   name: "OsInput",
 })
 export default class OsInput extends Vue {
+  @Prop({ default: false }) removable!: boolean;
+  @Prop({ required: false }) value!: any;
+
   os = null;
   arch = null;
   url = null;
+
+  get osInfo() {
+    const { os, arch, url } = this;
+    return { os, arch, url };
+  }
+
+  @Watch("os")
+  @Watch("arch")
+  @Watch("url")
+  onUpdateValue() {
+    this.$emit("input", this.osInfo);
+  }
 }
 </script>
--- example/frontend/src/views/SoftwareProposal.vue
+++ example/frontend/src/views/SoftwareProposal.vue
@@ -2,14 +2,54 @@
   <v-container>
     <h1>Add Software Proposal</h1>
 
-    <OsInput />
+    <form @submit.prevent="onSubmitSoftwareUpgradeProposal()">
+      <v-text-field label="Title" v-model="title" />
+      <v-text-field label="Description" v-model="description" />
+      <v-text-field label="Name" v-model="name" />
+      <v-text-field
+        label="Initial Deposit"
+        placeholder="Initial Deposit"
+        type="number"
+        v-model="initialDeposit"
+      />
+      <v-text-field label="Height" type="number" v-model="height" />
+
+      <v-row justify="space-between" class="mt-5 ml-0">
+        <h3>Operating Systems</h3>
+        <v-btn fab small color="success" @click="onAddSystem()">
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </v-row>
+
+      <OsInput
+        v-for="(os, idx) in systems"
+        :key="idx"
+        v-model="systems[idx]"
+        :removable="systems.length > 1"
+        @on:remove="onRemoveSystem(idx)"
+      />
+
+      <v-row justify="center" class="mt-4">
+        <v-btn
+          color="primary"
+          type="submit"
+          :disabled="loading"
+          :loading="loading"
+        >
+          Submit
+        </v-btn>
+      </v-row>
+    </form>
   </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import OsInput from "@/components/OsInput.vue";
-// import { submitSoftwareUpgradeProposal } from '@/utils/gov';
+import { submitSoftwareUpgradeProposal } from "@/utils/gov";
+import { BigNumber } from "ethers";
+import { SoftwareUpgradeProposal } from "@/types/cosmos/upgrade/v1beta1/upgrade";
+import Long from "long";
 
 @Component({
   name: "SoftwareProposal",
@@ -18,8 +58,54 @@ import OsInput from "@/components/OsInput.vue";
   },
 })
 export default class SoftwareProposal extends Vue {
-  // created() {
-  //   submitSoftwareUpgradeProposal()
-  // }
+  title = "";
+  description = "";
+  name = "";
+  height = 0;
+  initialDeposit = 0;
+  systems = [{ os: null, arch: null, url: null }];
+  loading = false;
+
+  onAddSystem() {
+    this.systems.push({ os: null, arch: null, url: null });
+  }
+
+  onRemoveSystem(i: number) {
+    this.systems.splice(i, 1);
+  }
+
+  onSubmitSoftwareUpgradeProposal() {
+    const { title, description, name, height, initialDeposit, systems } = this;
+
+    const info = systems.reduce((res, { os, arch, url }) => {
+      res[`${os}/${arch}`] = url;
+      return res;
+    }, {} as any);
+
+    const proposal: SoftwareUpgradeProposal = {
+      title,
+      description,
+      plan: {
+        name,
+        height: Long.fromNumber(height),
+        info: JSON.stringify(info),
+      },
+    };
+
+    this.loading = true;
+    submitSoftwareUpgradeProposal(
+      this.$store.state.config.tendermint_rpc,
+      proposal,
+      BigNumber.from(initialDeposit),
+      this.$store.state.config.tft_denom
+    )
+      .then((res) => console.log(res))
+      .catch((err) => {
+        console.log("Error", err);
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  }
 }
 </script>
