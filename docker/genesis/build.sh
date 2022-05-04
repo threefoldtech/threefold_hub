fa512a23dfe915df2b5b4cf0e743747ac8ed5787
OmarElawady <omarelawady1998@gmail.com>
2022-05-04 13:41:16 +0200

    copy missing node_key.json

--- docker/genesis/build.sh
+++ docker/genesis/build.sh
@@ -12,14 +12,16 @@ cleanup() {
 
 trap cleanup EXIT
 
-mkdir -p output || true
 
 docker run --env-file docker/genesis/env --name tfhub-genesis -ti tfhub-genesis
 docker export tfhub-genesis -o $tmp/tfhub.tar.gz
 tar -xf $tmp/tfhub.tar.gz -C $tmp
 popd
 
+mkdir -p output || true
+
 cp $tmp/root/.threefold_hub/config/genesis.json output/genesis.json
+cp $tmp/root/.threefold_hub/config/node_key.json output/node_key.json
 cp $tmp/root/go/bin/threefold_hubd output/therefold_hubd
 cp $tmp/root/.threefold_hub/config/app.toml output/app.toml
 cp $tmp/root/.threefold_hub/config/config.toml output/config.toml
